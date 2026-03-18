7d1af3a3113ed68621f14643ad8f7b63c66b94a0
Sairam9849 <sairam888899@gmail.com>
2026-03-18 19:09:23 +0530

    Trigger exception in home route
    Replace error message with a division by zero to trigger an exception.

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-import sys
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("ERROR: Something failed", file=sys.stderr)  # 
-    return "Testing Error"
+    return 1/0   
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
