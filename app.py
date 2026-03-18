bc6739a787cf1f02550a8c6bd4b910c5c9372dc1
Sairam9849 <sairam888899@gmail.com>
2026-03-18 19:03:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import sys
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("error occurred")   
+    print("ERROR: Something failed", file=sys.stderr)  # 
     return "Testing Error"
 
 app.run(host="0.0.0.0", port=5000)
