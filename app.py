c02cd4230f81f6aa866b917fc75e5b1e223a139c
Sairam9849 <sairam888899@gmail.com>
2026-03-18 18:58:08 +0530

    Remove duplicate print statement in home function

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("error occurred")   # 👈 THIS LINE ADD
+    print("error occurred")   
     return "Testing Error"
 
 app.run(host="0.0.0.0", port=5000)
